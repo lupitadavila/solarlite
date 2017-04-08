ca1ddba817486107d9d54d66f972f4baaf787030
Lupita <lupita.dav@gmail.com>
2017-04-08 11:16:06 -0700

    Main stying set up, font, color, etc

--- solar-report/db/schema.rb
+++ solar-report/db/schema.rb
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 20170408071811) do
     t.string   "password_digest"
     t.string   "city"
     t.string   "state"
-    t.string   "zip_code"
+    t.string   "country"
     t.string   "address"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
