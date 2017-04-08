d92ec10b8a004d619badb0181f3809c1e64c683b
Brian Bernheim <btbernheim@gmail.com>
2017-04-08 11:32:44 -0700

    Add Finance and Rank tables

--- solar-report/db/schema.rb
+++ solar-report/db/schema.rb
@@ -10,10 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170408071811) do
+ActiveRecord::Schema.define(version: 20170408182210) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "finances", force: :cascade do |t|
+    t.integer  "savings"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "ranks", force: :cascade do |t|
+    t.integer  "points"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
