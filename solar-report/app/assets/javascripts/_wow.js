94a36407910dd86d95b40aaf58fe4186d7d7b4ed
Lupita <lupita.dav@gmail.com>
2017-04-08 17:22:04 -0700

    So many changes, I didn't commit enough, sorry

--- solar-report/app/assets/javascripts/_wow.js
+++ solar-report/app/assets/javascripts/_wow.js
@@ -1,3 +1,12 @@
 $( document ).on('turbolinks:load', function() {
+  
   new WOW().init();
+
+  if( $(".button-collapse").length > 0 ){
+    $(".button-collapse").sideNav();
+  }
+
+  if( $('.parallax').length > 0 ){
+    $('.parallax').parallax();
+  }
 })
